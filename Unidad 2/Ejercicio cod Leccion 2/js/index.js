76e46373b4201f749a7a30f5edbaaa11f79002d4
MomoPipol <Leousset@gmail.com>
2020-02-07 13:44:41 -0300

    Correción Juan David

--- Unidad 2/Ejercicio cod Leccion 2/js/index.js
+++ Unidad 2/Ejercicio cod Leccion 2/js/index.js
@@ -6,10 +6,10 @@ document.getElementById('boton-perfil').addEventListener("click", function(){
     EntidadDeSalud: 'Salud Para Ti',
     Peso: 68,
     Estatura: 1.76,
-    FechaDeNac: New Date(Apr 02 1979)
-    Var UltimasConsultas = {
+    FechaDeNac: New Date(Apr 02 1979),
+    UltimasConsultas = {
         FechaConsulta: 'Fecha: 23 Junio 2016'
-        var consulta=['El paciente no presenta signos de dolor en las cicatrices de la cirugía','Se mencionan repetidos dolores en la zona abdominal'];
+        consulta=['El paciente no presenta signos de dolor en las cicatrices de la cirugía','Se mencionan repetidos dolores en la zona abdominal'];
       }
 
     }
