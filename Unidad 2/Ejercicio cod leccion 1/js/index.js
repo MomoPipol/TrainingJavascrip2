a784d7a06ffcfda505c652bfd54ba36f8a8d634d
MomoPipol <Leousset@gmail.com>
2020-02-06 21:29:18 -0300

    Objetivo logrado vamos. Pero hice la mitad.

--- Unidad 2/Ejercicio cod leccion 1/js/index.js
+++ Unidad 2/Ejercicio cod leccion 1/js/index.js
@@ -1,10 +1,21 @@
 
 document.getElementById('boton-receta1').addEventListener('click', function(){
+var ingrediente1="Claras de huevo"
+var ingrediente2="Azucar Blanca"
+var ingrediente3="Leche Condensada"
+var ingrediente4="Gelatina sin sabor"
+var ingrediente5="Limones"
+var cantidad1=5
+var cantidad2=150
+var cantidad3=200
+var cantidad4=1
+var cantidad5=4
 
-
-
-
-
+console.log("Primero debes adicionar las " + ingrediente1
++ " en un Bol y batir con los " + cantidad1
++ " gramos de " + ingrediente2 + "; luego debes adicionar los " + cantidad3 +" gramos de " + ingrediente3
++" y batir bien. Una vez termines con esto, adiciona el zumo de los " + ingrediente5 + " y la "+ ingrediente4
++" hidratada en agua tibia. Por último deposita la mezcla en moldes individuales y mételo a la nevera. ¡En 30 minutos to postre estará listo! ");
 })
 
 document.getElementById('boton-receta2').addEventListener('click', function(){
