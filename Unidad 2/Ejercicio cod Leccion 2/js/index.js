8f150eefd8e13988c8daa5ba8a7424f13a30fde6
MomoPipol <Leousset@gmail.com>
2020-02-07 13:52:08 -0300

    punto 7

--- Unidad 2/Ejercicio cod Leccion 2/js/index.js
+++ Unidad 2/Ejercicio cod Leccion 2/js/index.js
@@ -9,8 +9,11 @@ document.getElementById('boton-perfil').addEventListener("click", function(){
     FechaDeNac: New Date(Apr 02 1979),
     UltimasConsultas = {
         FechaConsulta: 'Fecha: 23 Junio 2016'
-        consulta=['El paciente no presenta signos de dolor en las cicatrices de la cirugía','Se mencionan repetidos dolores en la zona abdominal'];
+        consulta: ['El paciente no presenta signos de dolor en las cicatrices de la cirugía','Se mencionan repetidos dolores en la zona abdominal'];
       }
+      var HistoriaClinica1=["Fractura de fémur","3/11/2015"] // punto 7
+      var HistoriaClinica2=["Apendicitis","22/8/2015"]
+      var HistoriaClinica3=["Insuficiencia Renal","1/3/2013"]
 
     }
 })
