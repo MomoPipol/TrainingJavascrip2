bb54ccf7086520223c9ca4a277c0cef2216170bc
MomoPipol <Leousset@gmail.com>
2020-02-07 13:26:33 -0300

    13:26 ultimos cambios

--- Unidad 2/Ejercicio cod Leccion 2/js/index.js
+++ Unidad 2/Ejercicio cod Leccion 2/js/index.js
@@ -1,4 +1,25 @@
 
 document.getElementById('boton-perfil').addEventListener("click", function(){
-  
+  var Persona = {
+    nombre: 'Gabriel Santos',
+    ciudad: 'New York',
+    EntidadDeSalud: 'Salud Para Ti',
+    Peso: 68,
+    Estatura: 1.76,
+    FechaDeNac: New Date(Apr 02 1979)
+
+    libros: [
+      {
+        titulo: 'Africanus, el hijo del cónsul',
+        trilogia:'Escipion'
+      },
+      {
+        titulo: 'Las Legiones Malditas',
+        trilogia:'Escipion'
+      }
+    ]
+  }
+
+
+
 })
