75323c9e488c52aaa8517de2170fe91f0b94f8fa
MomoPipol <Leousset@gmail.com>
2020-02-10 17:35:37 -0300

    Selectores Abort venia bien igual

--- Unidad 3/Ejercicio de Codificación Leccion 1/codBase/js/app.js
+++ Unidad 3/Ejercicio de Codificación Leccion 1/codBase/js/app.js
@@ -1,3 +1,40 @@
+var usuario = {
+  nombre:"", usuario:"", contraseña:"",contraseñarep:"", email:"",date:"",descripcion:"",ciudad:"",direccion:"",cel:"",nacionalidad:"",genero:"", }
+
+ImprimirDatos : function(){
+console.log("Nombre Completo: "+This.nombre);
+console.log("Nombre de Usuario: "+this.usuario);
+console.log(""+This.contraseña);
+console.log(""+This.contraseñarep);
+console.log(""+This.email);
+console.log(""+This.date);
+console.log(""+This.descripcion);
+console.log(""+This.ciudad);
+console.log(""+This.direccion);
+console.log(""+This.cel);
+console.log(""+This.nacionalidad);
+console.log(""+This.genero);
+console.log(""+This.tipodepago;
+console.log(""+This.suscripcion);
+}
+//todo lo que sigue va adentro de la puta funcion
+nombre= document.getElementsByClassName('nombre');
+usuario= document.getElementsByClassName('nombre_usuario');
+contraseña= document.getElementsByClassName('password')
+contraseñarep=document.getElementsByName('password_repetida')
+ email=document.getElementsByName('email')
+ date=document.getElementsByName('fecha_nacimiento')
+ =document.getElementsByTagName('textarea')
+ =document.getElementsByTagName('tagName')
+ =document.getquerySelector('.fieldeset[0]')
+ =document.getquerySelector('.fieldeset[1]')
+ =document.getquerySelectorAll('.row')
+ =document.getquerySelectorAll('.row')
+tipodepago = document.getElementById('masculino');
+tipodepago = document.getElementById('femenino');
+suscripcion = document.getElementById('');
+suscripcion = document.getElementById('');
+
 
 document.getElementById('submit').addEventListener("click", function(){
 
