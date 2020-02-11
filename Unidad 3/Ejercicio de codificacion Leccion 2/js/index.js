78d88881c88b707090bfe0acf2521cf76d5b30de
MomoPipol <Leousset@gmail.com>
2020-02-11 14:23:25 -0300

    Div y fondo y display none al children

--- Unidad 3/Ejercicio de codificacion Leccion 2/js/index.js
+++ Unidad 3/Ejercicio de codificacion Leccion 2/js/index.js
@@ -8,9 +8,11 @@ function CambioFondo2(elemento) {
   if (document.getElementsByClassName('container-titulo').children[0]){document.getElementsByClassName('container-titulo').children[0].style.background="inherit";
   }
 }
-function CambioPaneles(elemento) {
-  document.querySelectorAll('.item')[0].style.width="4%";
 
+function CambioPaneles(elemento) {
+  document.querySelectorAll('div[class*=item]')[0].style.width="4%";
+  document.querySelectorAll('div[class*=item]')[0].style.background="#4d62d0";
+  document.querySelectorAll('div[class*=item]').children.display="none";
 }
 // la funcion CambioFondo(document.getElementsByClassName('container-titulo'))
 //function CambioFondo(elemento) {
